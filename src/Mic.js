7cb8e4a2cc385cff035bb77dfb5fe4be9855d8d9
kojakatsuma <katsuma.koja@ubiregi.com>
2019-12-14 14:54:27 +0900

    update

--- src/Mic.js
+++ src/Mic.js
@@ -2,10 +2,10 @@
 
 export default class Mic {
     constructor() {
+        // navigator.permissions.query()
         navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
             this.context = new AudioContext();
             this.analayzer = this.context.createAnalyser()
-            // this.analayzer.fftSize = 1024
             this.processor = this.context.createScriptProcessor(1024 * 2, 1, 1)
             this.input = this.context.createMediaStreamSource(stream)
             this.input.connect(this.analayzer)
@@ -25,27 +25,3 @@ export default class Mic {
         return this.res
     }
 }
-
-
-
-
-
-// navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
-//     const context = new AudioContext();
-//     const analayzer = context.createAnalyser()
-//     analayzer.smoothingTimeConstant = 0.2;
-//     analayzer.fftSize = 1024
-//     const processor = context.createScriptProcessor(1024 * 2, 1, 1)
-//     const input = context.createMediaStreamSource(stream)
-//     input.connect(analayzer)
-//     analayzer.connect(processor)
-//     processor.connect(context.destination)
-//     let spectrum = []
-//     let res = 0
-//     processor.onaudioprocess = () => {
-//         spectrum = new Uint8Array(analayzer.frequencyBinCount)
-//         analayzer.getByteFrequencyData(spectrum)
-//         res = spectrum.reduce((a, b) => Math.max(a, b))
-//     }
-//     return res
-// })
