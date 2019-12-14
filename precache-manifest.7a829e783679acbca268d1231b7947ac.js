a8f6ffa4b120efcddd6d0fc6226c90bd56c475df
kojakatsuma <katsuma.koja@ubiregi.com>
2019-12-15 01:00:14 +0900

    Updates

---
+++ precache-manifest.7a829e783679acbca268d1231b7947ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6598ba0c042a599ab42f0c68bacb886",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1a0d7c325b3f9047eea",
+    "url": "/static/js/2.5f9f7e01.chunk.js"
+  },
+  {
+    "revision": "e25222ef4993b16ce4830064af33d09f",
+    "url": "/static/js/2.5f9f7e01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f07f578763cd94360032",
+    "url": "/static/js/main.c066c3f1.chunk.js"
+  },
+  {
+    "revision": "30f8c12bd3bede5db209",
+    "url": "/static/js/runtime-main.f0978b62.js"
+  }
+]);
