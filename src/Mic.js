1c0810d7b7be939475076bc297368d8343bac0c4
kojakatsuma <katsuma.koja@ubiregi.com>
2019-12-16 09:57:31 +0900

    update

--- src/Mic.js
+++ src/Mic.js
@@ -5,23 +5,23 @@ export default class Mic {
         navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
             this.context = new AudioContext();
             this.input = this.context.createMediaStreamSource(stream)
-            this.analayzer = this.context.createAnalyser()
+            this.analyser = this.context.createAnalyser()
             this.processor = this.context.createScriptProcessor(1024 * 2, 1, 1)
-            this.input.connect(this.analayzer)
-            this.analayzer.connect(this.processor)
+            this.input.connect(this.analyser)
+            this.analyser.connect(this.processor)
             this.processor.connect(this.context.destination)
             this.spectrum = []
             this.res = 0
             this.processor.onaudioprocess = () => {
-                this.spectrum = new Uint8Array(this.analayzer.frequencyBinCount)
-                this.analayzer.getByteFrequencyData(this.spectrum)
+                this.spectrum = new Uint8Array(this.analyser.frequencyBinCount)
+                this.analyser.getByteFrequencyData(this.spectrum)
                 this.res = this.spectrum.reduce((a, b) => Math.max(a, b))
             }
         })
     }
 
     getLevel() {
-        return this.res
+        return this.res || 0
     }
 
     close() {
